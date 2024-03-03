f992df0b0de6245e10377de59766ee4442766314
Ethan Katnic <ethkatnic@gmail.com>
2024-03-02 16:07:04 -0800

    Separate base env settings from dev/stage/prod

--- poolpartybackend/poolpartybackend/settings/base.py
+++ poolpartybackend/poolpartybackend/settings/base.py
@@ -11,23 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+import environ
 
+env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 # Application definition
 
@@ -52,9 +44,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000"
-]
+CORS_ORIGIN_WHITELIST = []
 
 ROOT_URLCONF = "poolpartybackend.urls"
 
@@ -75,17 +65,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "poolpartybackend.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
---
+++ poolpartybackend/poolpartybackend/settings/dev.py
@@ -0,0 +1,19 @@
+# set DJANGO_SETTINGS_MODULE=poolpartybackend.settings.dev
+
+from .base import *
+from decouple import config
+
+DEBUG = True
+
+SECRET_KEY = config('SECRET_KEY')
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000"
+]
+
+DATABASES =  { 
+    "default": 
+        { 
+            "ENGINE": "django.db.backends.sqlite3", 
+            "NAME": BASE_DIR / "db.sqlite3", 
+    }
+}
