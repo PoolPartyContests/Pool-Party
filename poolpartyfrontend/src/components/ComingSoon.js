a532acae9c21d90f125ae92921d02e1a78acab42
Ethan Katnic <ethkatnic@gmail.com>
2024-03-05 13:21:06 -0800

    Update email Styling

--- poolpartyfrontend/src/components/ComingSoon.js
+++ poolpartyfrontend/src/components/ComingSoon.js
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import jsonp from "jsonp";
+import Alert from "@mui/material/Alert";
+import "../styles/base.css";
 
 function ComingSoon() {
   const [email, setEmail] = useState("");
+  const [alert, setAlert] = useState({ message: "", result: "" });
 
   const onSubmit = (e) => {
     e.preventDefault();
     const url =
       "https://gmail.us18.list-manage.com/subscribe/post-json?u=f626187bf48b35740a55f6f1f&amp;id=fe358a9230&amp;f_id=00dccbe0f0";
     jsonp(`${url}&EMAIL=${email}`, { param: "c" }, (_, data) => {
+      console.log(data);
       const { msg, result } = data;
-      // do something with response
-      alert(msg);
+      setAlert({ message: msg, result: result });
     });
+    setEmail("");
   };
 
   return (
@@ -23,18 +27,24 @@ function ComingSoon() {
           <p class="mt-4 text-lg">We're working on something awesome!</p>
         </div>
         <div class="mt-8">
+          {alert.message && (
+            <Alert severity={alert.result} style={{ margin: "20px" }}>
+              {alert.message}
+            </Alert>
+          )}{" "}
           <form action="#" method="post" class="flex items-center">
             <input
               type="email"
               name="email"
               placeholder="Enter your email"
-              class="py-2 px-4 bg-gray-800 rounded-l-md focus:outline-none"
+              class="subscribe-input"
               onChange={(e) => setEmail(e.target.value)}
+              value={email}
               required
             />
             <button
               type="submit"
-              class="py-2 px-4rounded-r-md hover:bg-blue-600 focus:outline-none "
+              className="subscribe-button"
               onClick={onSubmit}
             >
               Subscribe
