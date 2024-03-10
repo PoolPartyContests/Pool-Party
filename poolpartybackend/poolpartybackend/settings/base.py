55972ba8214be170e667d5c312f65ddebddc72d1
Ethan Katnic <ethkatnic@gmail.com>
2024-03-10 12:44:26 -0700

    Adjust base settings of django prod app

--- poolpartybackend/poolpartybackend/settings/base.py
+++ poolpartybackend/poolpartybackend/settings/base.py
@@ -17,7 +17,7 @@ env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
