6a7b7286199278b74f76c042243013fc1bb70a8b
Ethan Katnic <ethkatnic@gmail.com>
2024-03-10 17:18:53 -0700

    Try to fix

--- poolpartybackend/poolpartybackend/settings/prod.py
+++ poolpartybackend/poolpartybackend/settings/prod.py
@@ -1,5 +1,5 @@
 # set DJANGO_SETTINGS_MODULE=poolpartybackend.settings.prod
-
+import os
 from .base import *
 from decouple import config
 
@@ -9,3 +9,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 CORS_ORIGIN_WHITELIST = ['https://main.d3lcczce7y35ro.amplifyapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://main.d3lcczce7y35ro.amplifyapp.com']
 ALLOWED_HOSTS = ['*']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('RDS_DB_NAME'),
+        'USER': os.environ.get('RDS_USERNAME'),
+        'PASSWORD': os.environ.get('RDS_PASSWORD'),
+        'HOST': os.environ.get('RDS_HOSTNAME'),
+        'PORT': os.environ.get('RDS_PORT'),
+    }
+}
