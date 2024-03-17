c63e68abea382eb97bcbb75d45c59d17fa125fbd
Ethan Katnic <ethkatnic@gmail.com>
2024-03-17 11:06:46 -0700

    Finally fix backend

--- poolpartybackend/poolpartybackend/settings/prod.py
+++ poolpartybackend/poolpartybackend/settings/prod.py
@@ -8,14 +8,15 @@ DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
 CORS_ORIGIN_WHITELIST = ['https://main.d3lcczce7y35ro.amplifyapp.com','http://localhost:3000']
 CSRF_TRUSTED_ORIGINS = ['https://main.d3lcczce7y35ro.amplifyapp.com','http://localhost:3000']
-ALLOWED_HOSTS = ['*']
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('RDS_DB_NAME'),
-        'USER': os.environ.get('RDS_USERNAME'),
-        'PASSWORD': os.environ.get('RDS_PASSWORD'),
-        'HOST': os.environ.get('RDS_HOSTNAME'),
-        'PORT': os.environ.get('RDS_PORT'),
+ALLOWED_HOSTS = ['PoolPartyBackend-env.us-east-2.elasticbeanstalk.com']
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
