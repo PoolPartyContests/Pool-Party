bd12b2732c12bde94576f4202725dad3fefe50e2
Ethan Katnic <ethkatnic@gmail.com>
2024-03-18 14:16:22 -0700

    Continue configuring backend deploy

--- poolpartybackend/poolpartybackend/settings/prod.py
+++ poolpartybackend/poolpartybackend/settings/prod.py
@@ -1,14 +1,51 @@
 # set DJANGO_SETTINGS_MODULE=poolpartybackend.settings.prod
+# export $(sudo cat /opt/elasticbeanstalk/deployment/env | xargs)
 import os
 from .base import *
 from decouple import config
 
 DEBUG = False
 
+# https://aalvarez.me/posts/setting-up-elastic-beanstalk-health-checks-with-a-django-application/
+import requests
+def get_ec2_instance_ip():
+    """
+    Try to obtain the IP address of the current EC2 instance in AWS
+    """
+    # Get the session token
+    token_url = "http://169.254.169.254/latest/api/token"
+    token_headers = {"X-aws-ec2-metadata-token-ttl-seconds": "21600"}  # 6 hours
+    token_response = requests.put(token_url, headers=token_headers)
+    token = token_response.text
+
+    # Use the token to get the instance metadata
+    ip_url = "http://169.254.169.254/latest/meta-data/local-ipv4"
+    ip_headers = {"X-aws-ec2-metadata-token": token}
+    try:
+        ip_response = requests.get(ip_url, headers=ip_headers)
+        return ip_response.text
+    except requests.exceptions.ConnectionError:
+        return None
+    return ip
+
+# Configure SSL
+# https://blogs.ainomic.in/securing-your-django-website-on-aws-elastic-beanstalk-a-step-by-step-guide-to-https-implementation-6a8f02f16725
+
+
+AWS_LOCAL_IP = get_ec2_instance_ip()
+ALLOWED_HOSTS = [AWS_LOCAL_IP, 'PoolPartyBackend-env.us-east-2.elasticbeanstalk.com', 'api.poolpartycontests.com']
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
-CORS_ORIGIN_WHITELIST = ['https://main.d3lcczce7y35ro.amplifyapp.com','http://localhost:3000']
-CSRF_TRUSTED_ORIGINS = ['https://main.d3lcczce7y35ro.amplifyapp.com','http://localhost:3000']
-ALLOWED_HOSTS = ['PoolPartyBackend-env.us-east-2.elasticbeanstalk.com']
+CORS_ORIGIN_WHITELIST = [
+    'https://main.d3lcczce7y35ro.amplifyapp.com',
+    'https://api.poolpartycontests.com',
+    'https://poolpartycontests.com'
+]
+CSRF_TRUSTED_ORIGINS = [
+    'https://main.d3lcczce7y35ro.amplifyapp.com',
+    'https://api.poolpartycontests.com',
+    'https://poolpartycontests.com'
+]
 if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
--- poolpartybackend/poolpartybackend/urls.py
+++ poolpartybackend/poolpartybackend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from .views import health_check
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('api.urls')),
+    path('', health_check),
 ]
